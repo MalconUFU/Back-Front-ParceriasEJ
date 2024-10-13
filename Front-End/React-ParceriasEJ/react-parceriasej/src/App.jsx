831cbf9b993765d5628725757a27125637f207a7
otaviosenne <otavio.senne@gmail.com>
2024-10-13 16:20:33 -0300

    criacao de components

--- Front-End/React-ParceriasEJ/react-parceriasej/src/App.jsx
+++ Front-End/React-ParceriasEJ/react-parceriasej/src/App.jsx
@@ -1,16 +1,22 @@
-import './output.css'
+import Body from './components/body';
+import Footer from './components/footer';
+import Header from './components/header';
+import './output.css';
+
 
 function App() {
-
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-zinc-800">
-      <h1 className="text-4xl font-bold text-white">
-        Projeto Parcerias EJ
-      </h1>
-    </div>
+        <h1 className="text-4xl font-bold text-white">
+          Projeto Parcerias EJ
+        </h1>
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
