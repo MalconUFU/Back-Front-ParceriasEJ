a4ada5c6f4033eec93af37820ff62978eed5a06d
otaviosenne <otavio.senne@gmail.com>
2024-10-13 15:41:18 -0300

    React correto

---
+++ Front-End/React-ParceriasEJ/react-parceriasej/src/App.jsx
@@ -0,0 +1,16 @@
+import './output.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-zinc-800">
+      <h1 className="text-4xl font-bold text-white">
+        Projeto Parcerias EJ
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
